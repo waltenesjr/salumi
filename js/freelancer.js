9a65bb5cfef4cf212a7035dd7f301d8871d23d9a
waltenesjr <waltenesjr@live.com>
2019-05-09 16:36:56 -0300

    Correção de cores e disposição dos elementos

--- js/freelancer.js
+++ js/freelancer.js
@@ -17,12 +17,7 @@
 
   // Scroll to top button appear
   $(document).scroll(function() {
-/*    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {*/
-      $('.scroll-to-top').fadeIn();
-    /*} else {
-      $('.scroll-to-top').fadeOut();
-    }*/
+    $('.scroll-to-top').fadeIn();
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
